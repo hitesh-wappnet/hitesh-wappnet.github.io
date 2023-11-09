72a2f3dc40448df52c743af7e19d1d2208126c2c
Hitesh Rathod <hitesh.wappnet@gmail.com>
2023-11-09 18:21:10 +0530

    adding react site

---
+++ precache-manifest.3790bd26c5c8e563f1b55055446df5dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0096a6c138c8056a6d1450c7864cc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf99a4a7069152580893",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "13b4d3d921abbb3b0961",
+    "url": "/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "bf99a4a7069152580893",
+    "url": "/static/js/2.db21659b.chunk.js"
+  },
+  {
+    "revision": "13b4d3d921abbb3b0961",
+    "url": "/static/js/main.0c7069a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
