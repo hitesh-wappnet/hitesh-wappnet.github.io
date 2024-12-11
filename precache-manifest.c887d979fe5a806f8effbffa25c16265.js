54def1c58e2abbd1ebc5f6f231fb5d2f7ea281be
Hitesh Rathod <hitesh.wappnet@gmail.com>
2024-12-11 09:52:08 +0530

    build-1

---
+++ precache-manifest.c887d979fe5a806f8effbffa25c16265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53facae1f0d906698f268c9ba075cfb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c78b2399393f1f43850c",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "50e13ef8fef8a3a87e29",
+    "url": "./static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "c78b2399393f1f43850c",
+    "url": "./static/js/2.bb5052d3.chunk.js"
+  },
+  {
+    "revision": "50e13ef8fef8a3a87e29",
+    "url": "./static/js/main.169f50d1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "./static/media/logo.9f7e506d.svg"
+  }
+]);
